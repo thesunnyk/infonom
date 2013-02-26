edf79f8f503e97d0c544ec7ac1ff3e0f00534343
Sunny Kalsi <thesunnyk@gmail.com>
2013-02-27 01:24:45 +1100

    Added command to listen

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get('/hello.txt', function(req, res){
-  var body = 'Hello World';
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', body.length);
-  res.end(body);
+    res.send('Hello World');
 });
+
+app.listen(3000);
+console.log('Listening on port 3000');
