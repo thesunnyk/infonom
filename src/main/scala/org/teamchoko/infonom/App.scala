5685f52ff2d072cd7a1ffebd0bacde1fdcd11dd7
Sunny Kalsi <thesunnyk@gmail.com>
2014-07-24 23:01:23 +1000

    Now uses spray.

--- src/main/scala/org/teamchoko/infonom/App.scala
+++ src/main/scala/org/teamchoko/infonom/App.scala
@@ -1,12 +1,17 @@
-package teamchoko
+package org.teamchoko.infonom
+
+import akka.actor.{ActorSystem, Props}
+import akka.io.IO
+import spray.can.Http
 
 /**
  * Hello world!
  *
  */
-object App 
-{
-    def main(args: Array[String]):Unit={
-        println( "Hello World!" )
-    }
+object Main extends App with MySslConfiguration {
+  implicit val system = ActorSystem()
+
+  val handler = system.actorOf(Props[DemoService], name = "handler")
+
+  IO(Http) ! Http.Bind(handler, interface = "localhost", port = 8080)
 }
