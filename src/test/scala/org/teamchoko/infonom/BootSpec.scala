06456b2b1e39b14264ffd55661b0ee2af1430670
Sunny Kalsi <thesunnyk@gmail.com>
2014-07-30 23:44:41 +1000

    My shit increasingly resembles spray can example but no dice :(

--- src/test/scala/org/teamchoko/infonom/BootSpec.scala
+++ src/test/scala/org/teamchoko/infonom/BootSpec.scala
@@ -5,6 +5,6 @@ import org.scalatest._
 class AppSpec extends FlatSpec with Matchers {
 
   "App Message" should "be a greeting" in {
-    App.message should be ("Hello World")
+    Boot.message should be ("Hello World")
   }
 }
