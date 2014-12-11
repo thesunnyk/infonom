6fad874a7b7ac4e5672c8a1414598cb670448187
Sunny Kalsi <thesunnyk@gmail.com>
2014-12-11 23:53:10 +1100

    Version bumps, delete argonaut, go with H2 + doobie.
    doobie is locally built snapshot until 0.2 is released.

--- src/main/scala/org/teamchoko/infonom/DbConn.scala
+++ src/main/scala/org/teamchoko/infonom/DbConn.scala
@@ -1,115 +1,17 @@
 package org.teamchoko.infonom
 
 import scala.collection.immutable.Stream
-
 import java.net.URLEncoder
 import java.util.UUID
 import org.joda.time.DateTime
-
 import scalaz.stream.Process
 import scalaz.concurrent.Task
 import java.net.URI
 import org.http4s.{Response, Status, Method, Message, MediaType, Request, EntityBody, EntityDecoder, Uri, ParseException, ParseResult}
-import org.http4s.util.CaseInsensitiveString
-import org.http4s.client.Client
-import org.http4s.client.Client.Result
-import org.http4s.client.blaze.SimpleHttp1Client
+import doobie.util.transactor
+import scalaz.effect.IO
 
-object DesignDocs {
-  val ArticleData = Map {
-	  "all" -> Map {
-	    "map" ->
-	      """function(doc) {
-	    	|	if (doc.type === "article_data") {
-	    	|       emit(doc._id, doc);
-	    	|	}
-	    	|}""".stripMargin
-	  };
-	  "by_guid" -> Map {
-	    "map" ->
-	      """function(doc) {
-	    	|	if (doc.type === "article_data") {
-	    	|       emit(doc.guid, doc);
-	    	|	}
-	    	|}""".stripMargin
-	  };
-	  "bookmarked" -> Map {
-	    "map" ->
-	      """function(doc) {
-	    	|	if (doc.type === "article_data" && doc.bookmarked) {
-	    	|       emit(doc.date, doc);
-	    	|	}
-	    	|}""".stripMargin
-	  };
-	  "by_link_score" -> Map {
-	    "map" ->
-	      """function(doc) {
-	    	|	if (doc.type === "article_data") {
-	    	|       emit(doc.linkScore, doc);
-	    	|	}
-	    	|}""".stripMargin
-	  };
-	  "by_word_score" -> Map {
-	    "map" ->
-	      """function(doc) {
-	    	|	if (doc.type === "article_data") {
-	    	|       emit(doc.wordScore, doc);
-	    	|	}
-	    	|}""".stripMargin
-	  }
-  }
+object DbConn {
+  val db = transactor.DriverManagerTransactor[IO]("org.h2.Driver", "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "sa", "")
 
 }
-
-
-object CouchUri {
-  def sep = "/"
-  def encode(s: String) = URLEncoder.encode(s)
-  def path(parts: String*) = sep + parts.map(encode).mkString(sep)
-  def query(kv: String*) = if (kv.isEmpty) "" else {
-    "?" + kv.mkString("&")
-  }
-  def db(db: String) = path(db)
-  def view(db: String, designDocId: String, viewName: String, kvs: List[String]) =
-    path(db, "_design", designDocId, "_view", viewName) + query(kvs:_*)
-}
-
-// Use argonaut for JSON parsing.
-
-case class CouchEntity[T](item: T, id: UUID, rev: UUID, entityType: String)
-
-object Couchdb {
-  def apply(host: URI): ParseResult[Couchdb] = Uri.fromString(host.toASCIIString()).map(new Couchdb(_))
-}
-
-class Couchdb(host: Uri) {
-
-  def decodeArticle(msg: Message): Task[CouchEntity[Article]] = {
-    val id = new UUID(123L, 234L)
-    val rev = new UUID(123L, 234L)
-    val entityType = "article"
-    val article = Article("heading",
-                   "text",
-                   Textile(),
-                   true,
-                   None,
-                   None,
-                   DateTime.now(),
-                   URI.create("/test"))
-    Task.now(CouchEntity(article, id, rev, entityType))
-  }
-  
-  def articleDecoder(resp: Response): EntityDecoder[CouchEntity[Article]] = EntityDecoder(decodeArticle,
-      MediaType.`application/json`)
-  
-  def getArticles(count: Int = 10, offset: Int = 0) : Task[Result[CouchEntity[Article]]] = {
-    SimpleHttp1Client.decode(Request(Method.GET, host))(articleDecoder)
-  }
-
-  def getCompleteArticle(article: CouchEntity[Article]) : CompleteArticle = ???
-
-  def updateArticle(article: CouchEntity[Article], newArticle: Article) : CouchEntity[Article] = ???
-
-  def getCategories(count: Int = 10, offset: Int = 0) : Process[Task, CouchEntity[Category]] = ???
-}
-
---
+++ src/test/scala/org/teamchoko/infonom/DbConnSpec.scala
@@ -0,0 +1,13 @@
+package org.teamchoko.infonom
+
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+import java.net.URI
+
+class DbConnSpec extends FlatSpec with Matchers {
+  
+  "Pinging Google" should "have a greeting" in {
+    1 should equal(1)
+  }
+
+}
