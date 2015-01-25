2de2d489c44481dca0aaf2978606d1fe738374e4
Sunny Kalsi <thesunnyk@gmail.com>
2015-01-25 18:53:47 +1100

    Simple body echo on HTTP.

--- tomato/src/main/scala/org/teamchoko/infonom/tomato/MyService.scala
+++ tomato/src/main/scala/org/teamchoko/infonom/tomato/MyService.scala
@@ -7,13 +7,19 @@ import org.http4s.Request
 import org.http4s.Method
 import org.http4s.Status
 import org.http4s.ResponseBuilder
+import org.http4s.Response
+import scalaz.concurrent.Task
+import scodec.bits.ByteVector
 
 trait MyService {
-  val hello = "Hello"
+  
+  def makeString(body: ByteVector): String = new String(body.toArray) + "\n"
 
   def service(implicit executionContext: ExecutionContext): HttpService = HttpService {
     case Request(Method.GET, Uri(_, _, "/", _, _), _, _, _, _) =>
-      ResponseBuilder(Status.Ok, "pong")
+      ResponseBuilder(Status.Ok, "Server is up")
+    case Request(Method.POST, Uri(_, _, "/new/", _, _), _, _, doBody, _) =>
+      ResponseBuilder(Status.Ok, doBody.map(makeString))
   }
 
 }
--- tomato/src/test/scala/org/teamchoko/infonom/tomato/MyServiceSpec.scala
+++ tomato/src/test/scala/org/teamchoko/infonom/tomato/MyServiceSpec.scala
@@ -6,6 +6,5 @@ import org.scalatest.Matchers
 class MyServiceSpec extends FlatSpec with Matchers with MyService {
 
   "App Message" should "have a greeting" in {
-    hello should equal("Hello")
   }
 }
