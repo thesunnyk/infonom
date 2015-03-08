fe437b2591d8da1ac3e09b8e1678c30ab1d18bf8
Sunny Kalsi <thesunnyk@gmail.com>
2015-03-08 22:06:44 +1100

    Added H2 and some tests for DbConn

--- tomato/src/main/scala/org/teamchoko/infonom/tomato/DbConn.scala
+++ tomato/src/main/scala/org/teamchoko/infonom/tomato/DbConn.scala
@@ -3,42 +3,66 @@ package org.teamchoko.infonom.tomato
 import doobie.imports._
 import scalaz.concurrent.Task
 import java.util.Date
+import org.teamchoko.infonom.carrot.Articles.{Category, Author, Comment, Article}
+import org.teamchoko.infonom.carrot.Articles.{TextFilter, Html, Textile}
+import java.net.URI
+import org.joda.time.DateTime
 
 object DbConn {
-  val xa = DriverManagerTransactor[Task]("org.h2.Driver", "jdbc:h2:test.db", "sa", "")
-  
-  case class AuthorDb(name: String, email: Option[String], uri: Option[String])
+  def xa = DriverManagerTransactor[Task]("org.h2.Driver", "jdbc:h2:file:test.db", "sa", "")
 
-  def getAuthorById(aid: Long) = sql"select name, email, uri from author where id = $aid".query[AuthorDb].list
+  implicit val UriMeta: Meta[URI] =
+    Meta[String].nxmap(x => new URI(x), x => x.toASCIIString())
 
-  val getAuthors = sql"select name, email, uri from author".query[AuthorDb].list
+  implicit val DateTimeMeta: Meta[DateTime] =
+    Meta[Date].nxmap(x => new DateTime(x), x => x.toDate())
+
+  implicit val TextFilterMeta: Meta[TextFilter] =
+    Meta[String].nxmap(
+      x => x.toLowerCase match {
+        case x if x == "html" => Html()
+        case x if x == "textile" => Textile()
+      },
+      x => x match {
+        case Html() => "html"
+        case Textile() => "textile"
+      })
+
+  def getAuthorById(aid: Int) = sql"select name, email, uri from author where id = $aid".query[Author]
+
+  val getAuthors = sql"select name, email, uri from author".query[Author]
+
+  def createAuthor(a: Author) = sql"""
+    insert into author (name, email, uri)
+    values (${a.name}, ${a.email}, ${a.uri})
+  """.update
+
+  val lastVal = sql"select lastval()".query[Int].unique
   
-  val createAuthorTable: Update0 = sql"""
-      create table author {
-        id serial,
-        name varchar not null
+  def deleteAuthor(a: Author) = sql"delete from author where name = ${a.name}".update
+
+  val createAuthorTable = sql"""
+      create table author (
+        id serial primary key,
+        name varchar not null,
         email varchar,
         uri varchar
-      }
+      )
     """.update
 
-  case class CategoryDb(name: String, uri: Option[String])
+  val getCategory = sql"select name, uri from category".query[Category]
 
-  val getCategory = sql"select name, uri from category".query[CategoryDb].list
-
-  def getCategoryById(aid: Long) = sql"select name, uri from category where id = $aid".query[CategoryDb].list
+  def getCategoryById(aid: Long) = sql"select name, uri from category where id = $aid".query[Category]
 
   val createCategoryTable: Update0 = sql"""
       create table category {
         id serial,
-        name varchar,
-        uri varchar
+        name varchar not null,
+        uri varchar not null
       }
     """.update
-  
-  case class CommentDb(body: String, pubdate: Date)
 
-  def getCommentById(aid: Long) = sql"select body, pubdate from comment where id = $aid".query[CommentDb].list
+  def getCommentById(aid: Long) = sql"select body, pubdate from comment where id = $aid".query[Comment].list
 
   val createCommentTable: Update0 = sql"""
       create table comment {
@@ -65,14 +89,11 @@ object DbConn {
       }
     """.update
 
-  case class ArticleDb(heading: String, body: String, textfilter: String,
-    draft: Boolean, extract: String, pullquote: String, pubdate: Date, uri: String)
-
   def getArticleById(aid: Long) = sql"""
       select heading, body, textfilter, draft, extract, pullquote, pubdate, uri
       from article
       where id = $aid
-    """.query[ArticleDb].list
+    """.query[Article].list
 
   val createArticleTable: Update0 = sql"""
       create table article {
--- tomato/src/test/scala/org/teamchoko/infonom/tomato/DbConnSpec.scala
+++ tomato/src/test/scala/org/teamchoko/infonom/tomato/DbConnSpec.scala
@@ -1,10 +1,99 @@
 package org.teamchoko.infonom.tomato
 
+import doobie.imports._
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers
 import java.net.URI
+import scalaz.concurrent.Task
+import DbConn._
+import org.teamchoko.infonom.carrot.Articles.Author
 
 class DbConnSpec extends FlatSpec with Matchers {
+  def xaTest = DriverManagerTransactor[Task]("org.h2.Driver", "jdbc:h2:mem:test", "sa", "")
+
+
+  "Doobie Author SQL" should "create table correctly" in {
+    val analysis = DbConn.createAuthorTable.analysis.transact(xaTest).run
+    analysis.alignmentErrors should equal(Nil)
+  }
+
+  it should "typecheck delete authors" in {
+    val author = Author("things", None, Some(new URI("/author/things")))
+    val analysis = (for {
+      _ <- DbConn.createAuthorTable.run
+      analysisVal <- DbConn.deleteAuthor(author).analysis
+    } yield analysisVal).transact(xaTest).run
+
+    analysis.alignmentErrors should equal(Nil)
+  }
+
+  it should "typecheck create authors" in {
+    val author = Author("things", None, Some(new URI("/author/things")))
+    val analysis = (for {
+      _ <- DbConn.createAuthorTable.run
+      analysisVal <- DbConn.createAuthor(author).analysis
+    } yield analysisVal).transact(xaTest).run
+    analysis.alignmentErrors should equal(Nil)
+  }
+
+  it should "typecheck get authors" in {
+    val analysis = (for {
+      _ <- DbConn.createAuthorTable.run
+      analysisVal <- DbConn.getAuthors.analysis
+    } yield analysisVal).transact(xaTest).run
+    analysis.alignmentErrors should equal(Nil)
+  }
+
+  it should "typecheck get authors by Id" in {
+    val analysis = (for {
+      _ <- DbConn.createAuthorTable.run
+      analysisVal <- DbConn.getAuthorById(123).analysis
+    } yield analysisVal).transact(xaTest).run
+    
+    analysis.alignmentErrors should equal(Nil)
+  }
+
+  it should "leave an author in the db" in {
+    val author = Author("things", None, Some(new URI("/author/things")))
+
+    val fromDb: Author = (for {
+      table <- DbConn.createAuthorTable.run
+      thing <- DbConn.createAuthor(author).run
+      authId <- DbConn.lastVal
+      retrievedAuthor <- DbConn.getAuthorById(authId).unique
+    } yield retrievedAuthor).transact(xaTest).run
+
+    fromDb should equal(author)
+  }
+
+  it should "discern different authors by id" in {
+    val authorOne = Author("one", None, Some(new URI("/author/things")))
+    val authorTwo = Author("two", None, Some(new URI("/author/things")))
+
+    val fromDb: Author = (for {
+      _ <- DbConn.createAuthorTable.run
+      _ <- DbConn.createAuthor(authorOne).run
+      authId <- DbConn.lastVal
+      _ <- DbConn.createAuthor(authorTwo).run
+      retrievedAuthor <- DbConn.getAuthorById(authId).unique
+    } yield retrievedAuthor).transact(xaTest).run
+
+    fromDb should equal(authorOne)
+  }
   
+  it should "delete the correct author" in {
+    val authorOne = Author("one", None, Some(new URI("/author/things")))
+    val authorTwo = Author("two", None, Some(new URI("/author/things")))
+
+    val fromDb = (for {
+      _ <- DbConn.createAuthorTable.run
+      _ <- DbConn.createAuthor(authorOne).run
+      _ <- DbConn.createAuthor(authorTwo).run
+      _ <- DbConn.deleteAuthor(authorOne).run
+      retrievedAuthor <- DbConn.getAuthors.list
+    } yield retrievedAuthor).transact(xaTest).run
+
+    fromDb should equal(List(authorTwo))
+  }
 
 }
