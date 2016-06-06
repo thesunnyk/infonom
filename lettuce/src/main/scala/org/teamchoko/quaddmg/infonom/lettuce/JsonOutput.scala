c566914ea2c383adb44613953962cf36a2911d65
Sunny Kalsi <thesunnyk@gmail.com>
2016-06-06 22:11:24 +1000

    Don't overwrite file and add last modified time

--- lettuce/src/main/scala/org/teamchoko/quaddmg/infonom/lettuce/JsonOutput.scala
+++ lettuce/src/main/scala/org/teamchoko/quaddmg/infonom/lettuce/JsonOutput.scala
@@ -18,22 +18,26 @@ object JsonOutput {
 
   val log = LoggerFactory.getLogger("JsonOutput")
 
-  def getDirectory(uri: URI): StringError[File] = extractErrors(new File(uri).getParentFile())
-  
   def createDirectory(file: File): StringError[Unit] =
     extractErrors(if (!file.exists()) file.mkdirs() else true).flatMap(x => checkTrue(x, "Could not create directory"))
+
+  def newFile(str: String): StringError[File] = for {
+    file <- extractErrors(new File(str))
+    _ <- checkTrue(!file.exists(), s"File ${file} already exists")
+  } yield file
 
   def output(ca: CompleteArticleCase): StringError[Unit] = for {
     blogs <- extractErrors(new File("blogs"))
     _ = log.info("created blogs")
     _ <- createDirectory(blogs)
     _ = log.info("created dir")
-    file <- extractErrors(new File("blogs/" + ca.id + ".json"))
+    file <- newFile("blogs/" + ca.id + ".json")
     _ = log.info("created file")
     writer <- extractErrors(new FileWriter(file))
     _ = log.info("created writer")
     _ <- extractErrors(writer.write(ca.asJson.spaces2))
     _ <- extractErrors(writer.close())
+    _ = file.setLastModified(ca.article.pubDate.getMillis)
   } yield ()
 
 }
